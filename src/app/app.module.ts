cd6014ca5d8960afc0a894dc6b7d2eee6c66152a
NFelipao <nfelipe.gonzalez.96@gmail.com>
2023-01-04 14:48:53 -0300

    Third comm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+],
   imports: [
     BrowserModule,
     AppRoutingModule

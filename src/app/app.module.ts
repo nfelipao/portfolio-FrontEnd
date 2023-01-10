905e395435bd5fcdcf6f8499a274f78f3529bb07
NFelipao <nfelipe.gonzalez.96@gmail.com>
2023-01-10 03:25:39 -0300

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavComponent } from './components/nav/nav.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PortadaComponent,
+    SobremiComponent,
+    FormacionComponent,
+    ProyectosComponent,
+    TecnologiasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

1326b402640d05a23e39d3acf87bdcd89fc8697c
NFelipao <nfelipe.gonzalez.96@gmail.com>
2023-01-10 14:35:25 -0300

    v 0.2 angulardependstry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortadaComponent,
+    SobremiComponent,
+    FormacionComponent,
+    ProyectosComponent,
+    TecnologiasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/formacion/formacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {faCalendar} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-formacion',
+  templateUrl: './formacion.component.html',
+  styleUrls: ['./formacion.component.css']
+})
+export class FormacionComponent {
+  faCalendar = faCalendar;
+}
